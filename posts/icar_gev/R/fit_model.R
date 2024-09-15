8324ec6ba4732f9573aeecce34915be2acc6a1ca
bgautijonsson <bgautijonsson@gmail.com>
2024-09-15 10:17:28 +0000

    Adding a first post in series about copulas in Stan

--- posts/icar_gev/R/fit_model.R
+++ posts/icar_gev/R/fit_model.R
@@ -1,10 +1,10 @@
 if (interactive()) {
   box::use(
-    posts/icar_gev/r/prep_data
+    posts / icar_gev / r / prep_data
   )
 } else {
   box::use(
-    ./prep_data
+    . / prep_data
   )
 }
 
@@ -35,3 +35,42 @@ fit <- model$sample(
   refresh = 10,
   output_dir = output_dir
 )
+
+
+
+
+here("posts", "icar_gev", "results", "bym_results.csv") |>
+  read_csv() |>
+  mutate(
+    variable = str_c("<b>&", variable, ";</sub></b>") |>
+      str_replace("_", ";<sub>&")
+  ) |>
+  arrange(rep(1:3, times = 4)) |>
+  gt() |>
+  cols_hide(
+    columns = c(mean, sd, mad, rhat, ess_bulk, ess_tail)
+  ) |>
+  cols_label(
+    variable = "Parameter",
+    median = "Median",
+    q5 = "5th Percentile",
+    q95 = "95th Percentile"
+  ) |>
+  tab_header(
+    title = "BYM2 hyperparameters"
+  ) |>
+  fmt_markdown(
+    columns = variable
+  ) |>
+  fmt_number(
+    columns = mean:rhat,
+    decimals = 3
+  ) |>
+  fmt_number(
+    columns = ess_bulk:ess_tail,
+    decimals = 0
+  ) |>
+  tab_options(
+    table.font.size = 20,
+    table.width = pct(100)
+  )
